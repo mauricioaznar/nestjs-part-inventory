f4f1e5669b5337e6023e79cf36dbb8d3c1b988b0
mauricioaznar <mauricioaznar94@gmail.com>
2022-03-07 15:02:04 -0600

    initial commit

--- src/modules/seeder/modules/part-assignment/part-assignment.service.ts
+++ src/modules/seeder/modules/part-assignment/part-assignment.service.ts
@@ -45,5 +45,32 @@ export class PartAssignmentService {
       component_id: rawMaterials.creepvineSample.part_id,
       quantity: 2,
     });
+
+    // glass
+    await this.partComponentsService.addComponent({
+      parent_id: basicMaterials.glass.part_id,
+      component_id: rawMaterials.quartz.part_id,
+      quantity: 2,
+    });
+
+    // lubricant
+    await this.partComponentsService.addComponent({
+      parent_id: basicMaterials.lubricant.part_id,
+      component_id: rawMaterials.creepvineSeedCluster.part_id,
+      quantity: 1,
+    });
+
+    // plasteel ingot
+    await this.partComponentsService.addComponent({
+      parent_id: basicMaterials.plasteelIngot.part_id,
+      component_id: basicMaterials.titaniumIngot.part_id,
+      quantity: 1,
+    });
+
+    await this.partComponentsService.addComponent({
+      parent_id: basicMaterials.plasteelIngot.part_id,
+      component_id: rawMaterials.lithium.part_id,
+      quantity: 2,
+    });
   }
 }

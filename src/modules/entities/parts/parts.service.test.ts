bdbc1db7d7700aeb98f5422f827f681579b7b265
mauricioaznar <mauricioaznar94@gmail.com>
2022-03-10 14:39:28 -0600

    part service

--- src/modules/entities/parts/parts.service.test.ts
+++ src/modules/entities/parts/parts.service.test.ts
@@ -3,7 +3,7 @@ import { setupApp } from '../../common/__tests__/helpers/setup-app';
 import { PartCategoriesService } from '../part-categories/part-categories.service';
 import { PartsService } from './parts.service';
 
-describe('part category', () => {
+describe('part service', () => {
   let app: INestApplication;
   let partCategoriesService: PartCategoriesService;
   let partsService: PartsService;
@@ -34,5 +34,31 @@ describe('part category', () => {
     });
 
     expect(part.name).toBe('Part 1');
+  });
+
+  it('updates part name', async () => {
+    const part = await partsService.createPart({
+      name: 'Part 2',
+      image_url: null,
+      part_category_id: partCategory.part_category_id,
+    });
+
+    const updatedPart = await partsService.updatePart(part.part_id, {
+      name: 'Part 2 baby',
+      part_category_id: part.part_category_id,
+    });
+
+    expect(updatedPart.name).toBe('Part 2 baby');
+  });
+
+  it('fails when part doesnt exist', async () => {
+    const veryBigId = 1000000;
+
+    await expect(async () => {
+      await partsService.updatePart(veryBigId, {
+        name: 'Part 2 baby',
+        part_category_id: partCategory.part_category_id,
+      });
+    }).rejects.toThrow(/not found/i);
   });
 });
